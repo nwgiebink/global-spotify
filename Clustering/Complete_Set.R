80d6654acc27efeedc1cfc539ba9a7256f645df1
Sebastian Deimen <SebastianDeimen@gmx.de>
2020-05-03 23:29:15 -0700

    extention of clustering and new plot

--- Clustering/Complete_Set.R
+++ Clustering/Complete_Set.R
@@ -5,12 +5,16 @@ library(Rtsne)
 library(caret)
 library(cluster)
 library(dbscan)
+install.packages("BBmisc")
+library(BBmisc)
 # read the data in
-spot_in <- read_csv("C:/Users/Sebastian/Dropbox/A_UofA/INFO 523/final_project/global-spotify/data/spot_clean.csv")
+spot_in <- read_csv("/data/spot_clean.csv")
+
 
 # delete the attributes which should not be in
 spot <- spot_in %>% select(-track.id, -track.name, -track.track_number, -track.type, -track.album.album_type, 
                            -track.album.id,-track.album.name, -track.album.release_date, -track.album.total_tracks)
+
 
 # change mode to minor and major (major == 1, minor == 2)
 spot$mode <- if_else(spot$mode == 1, 1, 2)
@@ -151,6 +155,19 @@ dbscan_res$cluster
  
 # and now? don't know. Maybe bound the cluster back to the data and gain some insight?????????????
 
+spot_dbscan_cluster <- data.frame(spot_dbscan,dbscan_res$cluster,spot_dbscan_in$country)
+
+
+# do the summary 
+res_dbscan <- spot_dbscan_cluster %>% 
+  mutate(cluster = dbscan_res.cluster) %>%
+  mutate(country = spot_dbscan_in.country) %>%
+  group_by(cluster) %>%
+  do(the_summary = summary(.))
+
+
+res_dbscan$the_summary
+
 
 
 
@@ -193,3 +210,78 @@ abline(h=0.14, col ="red", lty=2)
 (res_col <- optics(gower_dist, eps=10, minPts = 10))
 plot(res_col)
 (res_col_d <- extractDBSCAN(res_col, eps_cl=0.14))
+
+
+
+#################################################################################################################
+spot_in <- read_csv("data/spot_clean.csv")
+# delete the attributes which should not be in
+spot <- spot_in %>% select(-track.id, -track.name, -track.track_number, -track.type, -track.album.album_type, 
+                           -track.album.id,-track.album.name, -track.album.release_date, -track.album.total_tracks)
+
+spot_social <- spot_in %>% select(gdp,freedom,density_sqkm,percent_internet_users,percent_urban,median_age,happiness,country)
+
+spot_social <- unique(spot_social)
+spot_social$country <- as.factor(spot_social$country)
+
+#gower distance matrix without country
+gower_dist <- daisy(spot_social,
+                    metric = "gower")
+
+
+summary(gower_dist)
+
+gower_mat <- as.matrix(gower_dist)
+
+# checking for number of clusters using silhouette coefficient
+
+sil <- c(NA)
+
+for(i in 2:22){
+  pam_fit <- pam(gower_mat, diss = TRUE, k = i)
+  
+  sil[i] <- pam_fit$silinfo$avg.width
+}
+
+plot(1:22, sil)
+lines(1:22, sil)
+
+# found k = 2 as best 
+
+pam_fit <- pam(gower_mat, k = 2, diss = TRUE)
+
+# add the cluster to the original data
+spot_cluster <- data.frame(spot_social, pam_fit$clustering)
+
+spot_cluster1 <- spot_cluster %>% filter(pam_fit.clustering == 1) %>% select(country)
+spot_cluster2 <- spot_cluster %>% filter(pam_fit.clustering == 2) %>% select(country)
+
+spot_social_clusters <- mutate(spot_in, cluster = ifelse(country %in% spot_cluster1$country,1,2))
+
+
+
+res2 <- spot_cluster %>% 
+  rename(cluster = pam_fit.clustering) %>%
+  group_by(cluster) %>%
+  do(the_summary = summary(.))
+
+res2$the_summary
+
+spot$track.explicit <- ifelse(spot$track.explicit == FALSE, 0,1)
+spot_scale <- spot %>% select(track.explicit, track.popularity,danceability,energy,loudness,mode, speechiness,acousticness, instrumentalness, liveness,valence, tempo, duration_min)
+
+# write the own scale funtion
+
+scale_self <- function(x){
+  norm=(x-min(x))/(max(x) - min(x))
+  return(norm)
+}
+
+spot_scale <- data.frame(apply(spot_scale, MARGIN=2, FUN=scale_self))
+
+library(reshape2)
+ggplot(melt(spot_scale), aes(variable, value, color = variable, fill = variable)) + 
+  geom_boxplot() + 
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1))
+ 
